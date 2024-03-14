0325dc71ac7557d872682085f72218e49b06458e
jhs921020 <111518792+jhs921020@users.noreply.github.com>
2024-03-15 01:27:09 +0900

    4.2 Create Account part Three

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import client from "../client";
 
 export default {
@@ -19,9 +20,18 @@ export default {
           ],
         },
       });
-      console.log(existingUser);
       // hash password
+      const uglyPassword = await bcrypt.hash(password, 10);
       // save and return the user
+      return client.user.create({
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          password: uglyPassword,
+        },
+      });
     },
   },
 };
