17c0fcc6d3f4007fc8e005c2ab9fbd274c1aa08b
jhs921020 <111518792+jhs921020@users.noreply.github.com>
2024-03-15 21:40:24 +0900

    4.9 authentication part Two

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import schema from "./schema";
 const PORT = process.env.PORT;
 const server = new ApolloServer({
   schema,
+  context: {
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzEwNTA0NTg4fQ.XQ82GOxUuoDVy4_YCRliiZTB_sN2iMiSJVNmJfQmjKQ",
+  },
 });
 
 server
