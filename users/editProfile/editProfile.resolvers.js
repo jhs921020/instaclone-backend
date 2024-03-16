7d0a4af2239e766cf17d3bdd3949dd194721518a
jhs921020 <111518792+jhs921020@users.noreply.github.com>
2024-03-16 14:25:59 +0900

    4.14 File Upload part One

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -4,7 +4,7 @@ import { protectedResolver } from "../users.utils";
 
 const resolverFn = async (
   _,
-  { firstName, lastName, username, email, password: newPassword },
+  { firstName, lastName, username, email, password: newPassword, bio },
   { loggedInUser, protectResolver }
 ) => {
   console.log(loggedInUser);
@@ -21,6 +21,7 @@ const resolverFn = async (
       lastName,
       username,
       email,
+      bio,
       ...(uglyPassword && { password: uglyPassword }), //password: uglyPassword 이 코드와 같음
     },
   });
